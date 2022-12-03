5d7ed3158a3ca51f057d611deec06a27e1ad3f7f
Caah Arch <caah135@hotmail.com>
2022-12-03 01:07:13 +0100

    Day 2, part 1

---
+++ day2/rock_paper_scissors.py
@@ -0,0 +1,36 @@
+
+# outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won)
+score_per_win = {"lose" : 0,
+    "draw" : 3,
+    "win": 6}
+
+
+import os
+script_dir = os.path.dirname(__file__)
+
+# read input into list
+with open(script_dir + "/input.txt", "r") as file:
+    inventory = file.read().strip()
+rounds = [elem.split(" ") for elem in inventory.split("\n")]
+# print(rounds)
+
+# modify input to use numbers instead of letters
+# Score 1 for Rock, 2 for Paper, and 3 for Scissors
+for round in rounds:
+    # A for Rock, B for Paper, and C for Scissors
+    round[0] = ord(round[0].lower()) - 96
+    # X for Rock, Y for Paper, and Z for Scissors
+    round[1] = ord(round[1].lower()) - 96 - 23
+# print(rounds)
+
+score = 0
+for round in rounds:
+    tmp = round[1]
+    if round [0] == round[1]:
+        tmp += 3
+    elif round[0] == round[1] - 1  or round[0] == round[1] + 2:
+        tmp += 6
+    print(f"input: {round}\tscore: {tmp}")
+    score += tmp
+
+print(f"SOLUTION 1: The score is {score}")
