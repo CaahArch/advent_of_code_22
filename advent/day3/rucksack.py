eb28f46601c9f7139576221494ce83f535afe627
Caah Arch <caah135@hotmail.com>
2022-12-04 22:09:47 +0100

    Solved day3! 🎉

--- advent/day3/rucksack.py
+++ advent/day3/rucksack.py
@@ -4,22 +4,38 @@ from advent.daily import read_input
 def split_in_half(rucksack: str):
     return rucksack[:len(rucksack)//2], rucksack[len(rucksack)//2:]
 
-def double_one_out(rucksack: list):
-    for c in rucksack[0]:
-        if c in rucksack[1]:
-            return c
+def double_one_out(rucksack: list) -> str:
+    same = set(rucksack[0])
+    for r in rucksack[1:]:
+        same = set([c for c in r if c in same])
+        if len(same) == 1:
+            return next(iter(same))
+    return same
 
 def item_to_priority(item):
+    if len(item) > 1:
+        item = item[0]
     if item.islower():
         return ord(item) - 96
     else:
         return ord(item) - 38
 
 ### Part 1 ###
-all_rucksacks = [split_in_half(items) for items in read_input().split('\n')]
-double_items = [double_one_out(r) for r in all_rucksacks]
+all_rucksacks = read_input().split('\n')
+double_items = [double_one_out(split_in_half(r)) for r in all_rucksacks]
 # print(double_items)
 priorities = [item_to_priority(i) for i in double_items]
 
 print(f"Solution 1 is {sum(priorities)}")
 
+### Part 2 ###
+def elves_form_groups(all_rucksacks):
+    "s -> (s0, s1, s2), (s1, s2, s3), (s2, s3, 4), ..."
+    a = iter(all_rucksacks)
+    return zip(a, a, a)
+    
+badges = [double_one_out(group) 
+    for group in elves_form_groups(all_rucksacks)]
+priorities = [item_to_priority(i) for i in badges]
+
+print(f"Solution 1 is {sum(priorities)}")
--- tests/test_day3.py
+++ tests/test_day3.py
@@ -1,12 +1,12 @@
 
-from advent.day3.rucksack import split_in_half, double_one_out, item_to_priority
+from advent.day3.rucksack import *
 
 def test_split_in_half():
     assert split_in_half("vJrwpWtwJgWrhcsFMMfFFhFp") == ("vJrwpWtwJgWr", "hcsFMMfFFhFp")
     assert split_in_half("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL") == ("jqHRNqRjqzjGDLGL", "rsFMfFZSrLrFZsSL")
     assert split_in_half("PmmdzqPrVvPwwTWBwg") == ("PmmdzqPrV", "vPwwTWBwg")
 
-def test_odd_one_out():
+def test_double_one_out():
     assert double_one_out(("vJrwpWtwJgWr", "hcsFMMfFFhFp")) == "p"
     assert double_one_out(("jqHRNqRjqzjGDLGL", "rsFMfFZSrLrFZsSL")) == "L"
     assert double_one_out(("PmmdzqPrV", "vPwwTWBwg")) == "P"
@@ -16,3 +16,22 @@ def test_item_to_priority():
     assert item_to_priority('z') == 26
     assert item_to_priority('A') == 27
     assert item_to_priority('Z') == 52
+
+def test_form_groups():
+    assert list(elves_form_groups([1, 2, 3, 4, 5, 6])) == [(1, 2, 3), (4, 5, 6)]
+    assert list(elves_form_groups([
+            "vJrwpWtwJgWrhcsFMMfFFhFp",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+            "PmmdzqPrVvPwwTWBwg",
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+            "ttgJtRGJQctTZtZT",
+            "CrZsJsPPZsGzwwsLwLmpwMDw"
+        ])) == [(
+            "vJrwpWtwJgWrhcsFMMfFFhFp",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+            "PmmdzqPrVvPwwTWBwg"
+        ), (
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+            "ttgJtRGJQctTZtZT",
+            "CrZsJsPPZsGzwwsLwLmpwMDw"
+        )]
