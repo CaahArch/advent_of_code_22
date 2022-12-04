64a8e3068759fe7faace953057f35fb75bac7498
Caah Arch <caah135@hotmail.com>
2022-12-04 20:43:56 +0100

    Gracefully fail if no file downloaded

--- advent/daily.py
+++ advent/daily.py
@@ -8,7 +8,10 @@ def read_input(file_path=None):
         script_path = sys._getframe(1).f_globals['__file__']
         script_dir = os.path.dirname(script_path)
         file_path = script_dir + "/input.txt"
-
+    
+    if not os.path.isfile(file_path):
+        print ("Please download the input files from https://adventofcode.com/2022")
+        exit(1)
     with open(file_path, "r") as file:
         contents = file.read().strip()
     return contents
