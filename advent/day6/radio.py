0fc92ff2959a772a5ce96a21058f669582197011
Caah Arch <caah135@hotmail.com>
2022-12-06 21:45:49 +0100

    Solved day 6 in 29 minutes!! 🎉

--- advent/day6/radio.py
+++ advent/day6/radio.py
@@ -1,17 +1,17 @@
 
 from advent.daily import read_input
 
-def find_first_header(received:str):
-    header = ""
+def find_first_header(received:str, len_marker:int):
 
-    for i in range(len(received) - 5):
-        header = received[i: i + 4]
-        if len(set(header)) == 4:
+    for i in range(len(received) - len_marker + 1):
+        header = received[i: i + len_marker]
+        if (duplicates := len(set(header))) == len_marker:
             print(f"{header} has no duplicates")
-            return i + 4
-        else:
-            print(f"{header} has duplicates")
+            return i + len_marker
+        # else:
+            # print(f"{header} has {len_marker - duplicates} duplicates")
 
 
 received = read_input()
-print(f"Solution 1: {find_first_header(received)}")
+print(f"Solution 1: {find_first_header(received, 4)}")
+print(f"Solution 2: {find_first_header(received, 14)}")
--- tests/test_day6.py
+++ tests/test_day6.py
@@ -1,8 +1,15 @@
 
 from advent.day6.radio import *
 
-def test_find_first_header():
-    assert find_first_header("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5
-    assert find_first_header("nppdvjthqldpwncqszvftbrmjlhg") == 6
-    assert find_first_header("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10
-    assert find_first_header("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11
+def test_find_start_package():
+    assert find_first_header("bvwbjplbgvbhsrlpgdmjqwftvncz", 4) == 5
+    assert find_first_header("nppdvjthqldpwncqszvftbrmjlhg", 4) == 6
+    assert find_first_header("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4) == 10
+    assert find_first_header("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4) == 11
+
+    
+def test_find_start_message():
+    assert find_first_header("bvwbjplbgvbhsrlpgdmjqwftvncz", 14) == 23
+    assert find_first_header("nppdvjthqldpwncqszvftbrmjlhg", 14) == 23
+    assert find_first_header("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14) == 29
+    assert find_first_header("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14) == 26
